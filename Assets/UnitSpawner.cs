1a9f1185169511954ae68d6ba691f3f071050dd1
NickSavino <nicksavino2@gmail.com>
2025-01-02 14:03:51 -0700

    Added basic unit spawning with preset navmesh agent path

---
+++ Assets/UnitSpawner.cs
@@ -0,0 +1,44 @@
+using System;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class UnitSpawner : MonoBehaviour
+{
+    private Camera _camera;
+
+    public GameObject selectedUnit;
+
+    private Vector3 _towerPos;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        _camera = Camera.main;
+
+        _towerPos = GameObject.Find("Castle").transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log(Input.mousePosition);
+            Ray castPoint = _camera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(castPoint, out hit, Mathf.Infinity))
+            {
+                if (hit.collider != null && hit.collider.gameObject.name.Contains("SpawnPoint") && Vector3.Distance(hit.point, gameObject.transform.position) < 10)
+                {
+                    Vector3 spawnPoint = gameObject.transform.position;
+                    spawnPoint.y = 1;
+                    GameObject unit = Instantiate(selectedUnit, spawnPoint, new Quaternion());
+                    unit.GetComponent<UnitController>()._destination = _towerPos;
+                }
+            }
+
+        }
+    }
+}
