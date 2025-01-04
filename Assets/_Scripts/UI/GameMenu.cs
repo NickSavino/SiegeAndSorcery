48ecd5d4113c9a193376ccf845d49a5b27e028f7
NickSavino <nicksavino2@gmail.com>
2025-01-03 17:43:40 -0700

    added more functionality to pause menu

--- Assets/_Scripts/UI/GameMenu.cs
+++ Assets/_Scripts/UI/GameMenu.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -5,10 +6,17 @@ public class GameMenu : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public Canvas pauseMenu;
+    public Canvas pauseMenuOptions;
+
+    private Canvas _currentScreen;
+
+    private Stack<Canvas> _navigationStack;
 
     public void Start()
     {
+        _navigationStack = new Stack<Canvas>();
         pauseMenu.enabled = false;
+        pauseMenuOptions.enabled = false;
     }
 
     public void Update()
@@ -16,6 +24,7 @@ public class GameMenu : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Escape))
         {
             pauseMenu.enabled = !pauseMenu.enabled;
+            _currentScreen = pauseMenu.enabled ? pauseMenu : null;
             Time.timeScale = Time.timeScale == 0 ? Time.timeScale = 1 : Time.timeScale = 0;
         }
     }
@@ -27,7 +36,18 @@ public class GameMenu : MonoBehaviour
 
     public void OnOptionsButtonClick()
     {
+        pauseMenuOptions.enabled = true;
+        _currentScreen.enabled = false;
+        _navigationStack.Push(_currentScreen);
+        _currentScreen = pauseMenuOptions;
+    }
 
+    public void OnBackButtonClick()
+    {
+        _currentScreen.enabled = false;
+        var prevScreen = _navigationStack.Pop();
+        prevScreen.enabled = true;
+        _currentScreen = prevScreen;
     }
 
     public void OnResumeButtonClick()
--- Assets/_Scripts/UI/MainMenu.cs
+++ Assets/_Scripts/UI/MainMenu.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Menu : MonoBehaviour
+public class MenuMenu : MonoBehaviour
 {
     public GameObject optionsScreen;
     public GameObject menuScreen;
