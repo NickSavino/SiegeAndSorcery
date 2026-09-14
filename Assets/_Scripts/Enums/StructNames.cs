b71855ee69a99509c84e75e8ab3ba04aec0f57bc
desobob11 <desmond.obrien@ucalgary.ca>
2026-09-13 22:09:13 -0600

    ontrolg roups added

--- Assets/_Scripts/Enums/StructNames.cs
+++ Assets/_Scripts/Enums/StructNames.cs
@@ -5,6 +5,10 @@ public class STRUCTS_NAMES
     private STRUCTS_NAMES(string value) { Value = value; }
     public static string UNIT_COLLIDER { get { return new STRUCTS_NAMES("UnitCollider").Value; } }
     public static string STRUCTURE_MANAGER { get { return new STRUCTS_NAMES("StructureManager").Value; } }
+
+    public static string UNIT_PATH { get { return new STRUCTS_NAMES("UnitPath").Value; } }
+    public static string ACTIVE_PATH { get { return new STRUCTS_NAMES("ActivePath").Value; } }
+    public static string TEMP_PATH { get { return new STRUCTS_NAMES("TempPath").Value; } }
     public override string ToString()
     {
         return Value;
