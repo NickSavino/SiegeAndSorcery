6510f4410033fb359e0163d381e2103216eb651c
Nicola Savino <77707655+NickSavino@users.noreply.github.com>
2026-09-15 19:36:17 -0600

    Feat/level selector (#28)
    * Created UIController For Defender and modified StrcuturePlacement and WallBuilding Controllers. removed no longer needed WallScene
    
    * Create README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    added contact
    
    * Update README.md
    
    * Update README.md
    
    * unit controller null reference exception (#24) (#26)
    
    Co-authored-by: Circinus10 <kirbygarcia10@gmail.com>
    
    * feat: deleted broken prefab
    
    * fix: fixed wallbuildingcontroller
    
    * unrelated formatting cleanup
    
    * unrelated formatting cleanup
    
    * unrelated prefab change
    
    * updated package
    
    * feat: added level selector
    
    * refactor: renamed ScreenControllers to just be Screens
    
    * fix: added personal workspace settings to get ignore
    
    ---------
    
    Co-authored-by: desobob11 <67239552+desobob11@users.noreply.github.com>
    Co-authored-by: desobob11 <desmond.obrien@ucalgary.ca>
    Co-authored-by: seantan88 <65330251+seantan88@users.noreply.github.com>
    Co-authored-by: Circinus10 <kirbygarcia10@gmail.com>

--- Assets/_Scripts/UI/GameMenu.cs
+++ Assets/_Scripts/UI/GameMenu.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using _Scripts.Controllers;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -32,7 +33,7 @@ public class GameMenu : MonoBehaviour
     public void OnMainMenuButtonClick()
     {
         Time.timeScale = 1;
-        SceneLoadingController.instance.LoadScene("MainMenuScene");
+        SceneLoadingController.Instance.LoadScene("MainMenuScene");
     }
 
     public void OnOptionsButtonClick()
