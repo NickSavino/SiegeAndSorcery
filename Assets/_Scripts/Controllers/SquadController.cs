e03bf6863abd42b2d38e44fe5e9ee122c638c794
desobob11 <desmond.obrien@ucalgary.ca>
2026-09-15 23:15:41 -0600

    Static multi unit destination setting working

--- Assets/_Scripts/Controllers/SquadController.cs
+++ Assets/_Scripts/Controllers/SquadController.cs
@@ -4,6 +4,9 @@ using UnityEngine.UI;
 using UnityEngine.UIElements;
 using System.Collections.Generic;
 using System;
+using UnityEngine.InputSystem;
+using Cinemachine.Utility;
+using static UnityEngine.UI.Image;
 
 public class SquadController : MonoBehaviour
 {
@@ -16,6 +19,8 @@ public class SquadController : MonoBehaviour
 
     [field: SerializeField]
     public int _squadSize = 12;
+
+    Vector3 anchor = Vector3.zero;
 
     [field: SerializeField]
     public GameObject _unitTypeTest;
@@ -42,7 +47,7 @@ public class SquadController : MonoBehaviour
     // Update is called once per framew
     void Update()
     {
-        SetUnitsDestination();
+        SetUnitsDestinations();
     }
 
 
@@ -61,104 +66,93 @@ public class SquadController : MonoBehaviour
     List<GameObject> InitUnits() {
         List<GameObject> units = new List<GameObject>();
         for (int i = 0; i < _squadSize; ++i) {
-            units.Add(Instantiate(_unitTypeTest));
+            units.Add(Instantiate(_unitTypeTest, this.transform, true));
         }
         return units;
     }
 
-    int IndexConvert(int i, int j) {
-        return (i * SIZE_COLS) + j;
+    int IndexConvert(int i, int j, ROW_COL rowsCols) {
+        return (i * rowsCols.cols) + j;
     }
 
-    void SetUnitsDestination() {
+
+    /**
+     *  Handles sqaud destination selection by users
+     * 
+     *  Determines if left click (default size) or dynamic sizing
+     */
+
+    void SetUnitsDestinations() {
+
+        // Get first mouse position from first clock (top corner of draggable grid)
         if (Input.GetMouseButtonDown(1)) {
-           // RaycastHit hit;
-           // Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            anchor = Input.mousePosition;
+        }
+
+        // if holding down
+        if (Input.GetMouseButton(1)) {
+
+            // end of draggable grid is CURRENT mouse position this frame
+            Vector3 pivot = Input.mousePosition;
+
+            Vector3 diff = (anchor - pivot);
+
+            ROW_COL rowsCols;
+            Vector3 origin;
+
+            // case of just a click no hold, default size
+            if (diff == Vector3.zero) {
+                rowsCols = new ROW_COL(SIZE_ROWS, SIZE_COLS);
+                origin = Input.mousePosition;
+            }
+            else {      // otherwise draggable sizing
+                rowsCols = GetOptimalShape(anchor, pivot);
+                origin = anchor;
+            }
+
+            // world units to screen pixels
             float pixelsPerUnit = Screen.height / (Camera.main.orthographicSize * 2f);
+            float width = rowsCols.cols * pixelsPerUnit;
+            float height = rowsCols.rows * pixelsPerUnit;
 
-            float width = SIZE_COLS * pixelsPerUnit;
-            float height = SIZE_ROWS * pixelsPerUnit;
+            // offset, center on cursor
+            if (diff == Vector3.zero) {
+                origin -= new Vector3(width / 2f, height / 2f, 0);
+            }
 
-
-
-            Vector3 origin = Input.mousePosition - new Vector3(width / 2f, height / 2f, 0);
             Vector3 iter = new Vector3(origin.x, origin.y, origin.z);
 
-            GameObject cont = null;
-            for (int i = 0; i < SIZE_ROWS; ++i) {
-                for (int j = 0; j < SIZE_COLS; ++j) {
-                    RaycastHit hit;
-                    Ray ray = Camera.main.ScreenPointToRay(iter);
-                    if (Physics.Raycast(ray, out hit)) {   // _currentPosts[0] is initial post
-
-                        Vector3 destination = hit.point;
-                        //      destination.y = spheres[0].gameObject.transform.position.y;  // keep y axis unaffected
-                        // foreach (GameObject cont in spheres) {
-
-                        cont = spheres[IndexConvert(i, j)];
-                            
-                           cont.transform.position = destination;
-
-                       // }
-                    }
-                    iter += new Vector3(pixelsPerUnit * cont.transform.lossyScale.x, 0, 0);
-                }
-                iter += new Vector3(0, pixelsPerUnit * cont.transform.lossyScale.y, 0);
-                iter.x = origin.x;
-            }
+            // Cast the actual rays to get the coords we need to set each destination
+            CastRaysDraggableSelection(origin, rowsCols, pixelsPerUnit);
         }
     }
+   
 
-    void SetUnitsDestinationScaled() {
-        if (Input.GetMouseButtonDown(1)) {
-            Vector3 anchor = Input.mousePosition;
+    void CastRaysDraggableSelection(Vector3 origin, ROW_COL rowsCols, float pixelsPerUnit) {
+        Vector3 iter = new Vector3(origin.x, origin.y, origin.z);
 
-            if (Input.GetMouseButton(1)) {
+        GameObject cont = null;
+        for (int i = 0; i < rowsCols.rows; ++i) {
+            for (int j = 0; j < rowsCols.cols; ++j) {
+                RaycastHit hit;
+                Ray ray = Camera.main.ScreenPointToRay(iter);
+                if (Physics.Raycast(ray, out hit)) {   // _currentPosts[0] is initial post
 
-                Vector3 pivot = Input.mousePosition;
+                    Vector3 destination = hit.point;
 
-                ROW_COL rowsCols = getOptimalShape(anchor, pivot);
+                    cont = spheres[IndexConvert(i, j, rowsCols)];
 
-                // RaycastHit hit;
-                // Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-                float pixelsPerUnit = Screen.height / (Camera.main.orthographicSize * 2f);
-
-                float width = SIZE_COLS * pixelsPerUnit;
-                float height = SIZE_ROWS * pixelsPerUnit;
-
-
-
-                Vector3 origin = Input.mousePosition - new Vector3(width / 2f, height / 2f, 0);
-                Vector3 iter = new Vector3(origin.x, origin.y, origin.z);
-
-                GameObject cont = null;
-                for (int i = 0; i < SIZE_ROWS; ++i) {
-                    for (int j = 0; j < SIZE_COLS; ++j) {
-                        RaycastHit hit;
-                        Ray ray = Camera.main.ScreenPointToRay(iter);
-                        if (Physics.Raycast(ray, out hit)) {   // _currentPosts[0] is initial post
-
-                            Vector3 destination = hit.point;
-                            //      destination.y = spheres[0].gameObject.transform.position.y;  // keep y axis unaffected
-                            // foreach (GameObject cont in spheres) {
-
-                            cont = spheres[IndexConvert(i, j)];
-
-                            cont.transform.position = destination;
-
-                            // }
-                        }
-                        iter += new Vector3(pixelsPerUnit * cont.transform.lossyScale.x, 0, 0);
-                    }
-                    iter += new Vector3(0, pixelsPerUnit * cont.transform.lossyScale.y, 0);
-                    iter.x = origin.x;
+                    cont.transform.position = destination;
                 }
+                iter += new Vector3(pixelsPerUnit * cont.transform.lossyScale.x, 0, 0);
             }
+            iter += new Vector3(0, pixelsPerUnit * cont.transform.lossyScale.y, 0);
+            iter.x = origin.x;
         }
     }
 
 
-    ROW_COL getOptimalShape(Vector3 anchor, Vector3 pivot) {
+    ROW_COL GetOptimalShape(Vector3 anchor, Vector3 pivot) {
         float pixelsPerUnit = Screen.height / (Camera.main.orthographicSize * 2f);
         float xDistance = Math.Abs(anchor.x - pivot.x);
 
@@ -166,9 +160,9 @@ public class SquadController : MonoBehaviour
 
 
 
-        int numRows = Math.Min(1,  (int) (xDistance / pixelsPerSphere));
+        int numRows = Math.Max(1,  _squadSize - ((int) (xDistance / pixelsPerSphere)));
         int numCols = 0;
-        for (int i = numRows; i >= 1; ++i) {
+        for (int i = numRows; i >= 1; --i) {
             if (_squadSize % i == 0) {
                 numCols = _squadSize / i;
                 numRows = i;
